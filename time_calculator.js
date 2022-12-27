33a0de41c3e832e187e866edbb394f961b7f8290
SatyamSSJ10 <60954779+SatyamSSJ10@users.noreply.github.com>
2022-12-27 14:59:20 +0530

    Update time_calculator.js

--- time_calculator.js
+++ time_calculator.js
@@ -14,12 +14,35 @@ class Task{
         var time = this.task[counter-1][2].split(":");
         let hour = this.time.getHours();
         let minutes = this.time.getMinutes();
-        return `${Math.abs(Number(time[0])-hour)}:${Math.abs(Number(time[1])-minutes)} Left`
-    }
-    time_left(counter){
-        console.log(add_time(this.task[counter-1][2]));
+        if (Number(time[1]) > minutes){
+            hour--;
+            var real_min = 60 - Number(time[1]) + Number(minutes);
+        }
+        else{
+            var real_min = Math.abs(Number(time[1])-minutes);
+        }
+        return `${Math.abs(Number(time[0])-hour)}:${real_min}`;
     }
     print_task(){
-        
+        for (const x of this.task){
+            console.log(x);
+        }
+    }
+    alarm_user(){ //Notify User
+        for(x=1;x<=this.counter;x++){
+            if ("0:0"==this.calc_time(x)){
+                console.log("time for Task :");
+                console.log(this.task[x-1][1]);
+            }    
+        }
+    }
+    delete_task(){ //Remove Task
+
+    }
+    shift_task(){ //Change Task Time
+
+    }
+    change_priority(){ //change priority of task
+
     }
 }
