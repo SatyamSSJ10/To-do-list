5b1388c354df51aea5e98bc63765eb70e82650f8
SatyamSSJ10 <60954779+SatyamSSJ10@users.noreply.github.com>
2023-01-02 18:21:12 +0530

    Update time_calculator.js

--- time_calculator.js
+++ time_calculator.js
@@ -1,14 +1,15 @@
+const input = require('prompt-sync')();
 class Task{
-    contructor(name){
-        this.username=name
-        this.task=[]
-        this.counter = 0
+    constructor(name){
+        this.username=name;
+        this.task=[];
+        this.counter = 0;
         this.time = new Date();
     }
     add_task(task,time){
         this.counter++;
         this.task.push([Number(this.counter),String(task),String(time)]);
-        console.log(`Appended: ${String(task)} for ${time}`);
+        console.log(`Appended: ${String(task)} for time = ${time}`);
     }
     calc_time(counter){
         var time = this.task[counter-1][2].split(":");
@@ -24,8 +25,11 @@ class Task{
         return `${Math.abs(Number(time[0])-hour)}:${real_min}`;
     }
     print_task(){
+        if (this.counter == 0){
+            console.log("No task")
+        }
         for (const x of this.task){
-            console.log(x);
+            console.log(x[0] + " . " + x[1] + " at " + x[2]);
         }
     }
     alarm_user(){ //Notify User
@@ -36,13 +40,52 @@ class Task{
             }    
         }
     }
-    delete_task(){ //Remove Task
-
-    }
     shift_task(){ //Change Task Time
-
+        for(const x of this.task){
+                x[0] = Number(this.task.indexOf(x)) + 1;
+            }    
+        }
+    delete_task(pos){ //Remove Task
+        this.task.splice(pos-1,1);
+        console.log("Updated task list: ");
+        this.counter--;
+        this.shift_task();
+        this.print_task();
     }
-    change_priority(){ //change priority of task
-
+    
+    change_priority(pos,newone){ //change priority of task
+        let temp = this.task.splice(pos-1,1);
+        this.task.splice(newone-1,0,temp);
+        console.log("Updated task list: ");
+        this.print_task();
     }
+}
+var flag = true;
+var val=0;
+while (flag){
+    console.log("Select one Option\n");
+    console.log("1. Add User\n2. Print Task\n3. Add Task \n4. Delete Task\n5. Exit");
+    val = Number(input(""))
+    switch (val-1) {
+        case 0:
+            var user = new Task(String(input("Name is: ")));
+            console.log("Declared User: " + user.username);
+            break;
+        case 1:
+            console.log("Printing Task: \n")
+            user.print_task();
+            break;
+        case 2:
+            console.log("Enter:")
+            user.add_task(String(input("Task: ")),String(input("Time: ")));
+            break;
+        case 3:
+            user.print_task();
+            user.delete_task(Number(input("Delete Task Number? ")))
+            user.shift_task();
+            break;
+        case 4:
+            flag = false;
+            break;
+        }
 }
